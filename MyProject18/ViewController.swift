90bd294796aa61057588da0d7dc027b19f925c8c
GeorgEvseyev <evseyevgeorg@gmail.com>
2023-05-03 21:24:47 +0300

    Update

--- MyProject18/ViewController.swift
+++ MyProject18/ViewController.swift
@@ -5,15 +5,16 @@
 //  Created by Георгий Евсеев on 13.09.22.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        for i in 1 ... 1000 {
+            print("Got number \(i).")
+        }
     }
-
-
 }
-
